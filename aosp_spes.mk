da07a4dcdfba3068c9f0e81a049d14dec1591bcf
imannig <zahranansaria@gmail.com>
2022-09-23 16:16:53 +0700

    spes: Adapt to elixir
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- aosp_spes.mk
+++ aosp_spes.mk
@@ -11,15 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Evolution X stuff.
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common ProjectElixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_MINI_GAPPS := true
-TARGET_ENABLE_BLUR := false
+TARGET_GAPPS_ARCH := arm64
 
 # Product Specifics
-PRODUCT_NAME := evolution_spes
+PRODUCT_NAME := aosp_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -31,3 +29,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="spes-user 11 RKQ1.211001.001 V13.0.15.0.RGCMIXM release-keys"
 
 BUILD_FINGERPRINT := Redmi/spes/spes:11/RKQ1.211001.001/V13.0.15.0.RGCMIXM:user/release-keys
+
+# Elixir
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ELIXIR_MAINTAINER := Fauzan & Zahran
+ELIXIR_BUILD_TYPE := OFFICIAL
